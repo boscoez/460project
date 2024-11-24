e99442de30cc98a2a6a0db4ed2ed1bf8467e62cd
Howey <hieuluong1311@gmail.com>
2024-11-24 12:00:27 -0800

    Added the new password and confirm password in the app

--- app/src/main/java/com/example/ezchat/LoginUserNameActivity.java
+++ app/src/main/java/com/example/ezchat/LoginUserNameActivity.java
@@ -2,26 +2,40 @@ package com.example.ezchat;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import android.provider.MediaStore;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ezchat.databinding.ActivityLoginUserNameBinding;
 import com.example.ezchat.model.UserModel;
+import com.example.ezchat.utils.Constants;
 import com.example.ezchat.utils.FirebaseUtil;
+import com.example.ezchat.utils.PreferenceManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 import java.util.Objects;
 
+
 public class LoginUserNameActivity extends AppCompatActivity {
-    EditText usernameInput;
-    Button letMeInBtn;
-    ProgressBar progressBar;
-    String phoneNumber;
-    UserModel userModel;
+    private ActivityLoginUserNameBinding binding;
+
+    private PreferenceManager preferenceManager;
+
+
+
 
     /**
      * Initializes the username setup activity, sets up UI components, and retrieves any existing username.
@@ -32,80 +46,99 @@ public class LoginUserNameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_user_name);
 
-        usernameInput = findViewById(R.id.login_username);
-        letMeInBtn = findViewById(R.id.login_code_btn);
-        progressBar = findViewById(R.id.login_progress_bar);
-        phoneNumber = Objects.requireNonNull(getIntent().getExtras()).getString("phone");
-        getUsername();
 
-        letMeInBtn.setOnClickListener((v -> setUsername()));
+
+        binding = ActivityLoginUserNameBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
     }
 
-    /**
-     * Validates and sets the username for the user. If a UserModel doesn't already exist, it creates one,
-     * saves it in Firebase, and navigates to the MainActivity on success.
-     */
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v ->onBackPressed());
+        binding.loginCodeBtn.setOnClickListener(v->{
+            if(isValidateSignUpDetails()) {
+                SignUp();
+            }
+        });
 
-    void setUsername() {
+    }
 
-        String username = usernameInput.getText().toString();
-        if (username.isEmpty() || username.length() < 3) {
-            usernameInput.setError("Username length must be 3+ characters. ");
-            return;
-        }
-        setInProgress(true);
-        if(userModel != null){
-            userModel.setUsername(username);
-        } else {
-            userModel = new UserModel(phoneNumber, username, Timestamp.now(), FirebaseUtil.currentUserId());
-        }
+    public void showToast(String message) {
 
-        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                setInProgress(false);
-                if (task.isSuccessful()) {
-                    Intent intent = new Intent(LoginUserNameActivity.this, MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void SignUp() {
+        loading(true);
+        //Post to Firebase
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+        HashMap<String, String> user = new HashMap<>();
+        user.put(Constants.KEY_USERNAME,binding.loginUsername.getText().toString());
+        user.put(Constants.KEY_PASSWORD,binding.loginPassword.getText().toString());
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    loading(false);
+                    preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
-                }
-            }
-        });
+                }).addOnFailureListener(exception -> {
+                    loading(false);
+                    showToast(exception.getMessage());
+                });
+
+
+
     }
 
-    /**
-     * Retrieves the existing username from Firebase Firestore if it exists and populates the input field.
-     */
+    private Boolean isValidateSignUpDetails() {
 
-    void getUsername(){
-        setInProgress(true);
-        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(task -> {
-            setInProgress(false);
-            if(task.isSuccessful()){
-                UserModel userModel = task.getResult().toObject(UserModel.class);
-                if(userModel != null){
-                    usernameInput.setText(userModel.getUsername());
-                }
-            }
-        });
-    }
 
-    /**
-     * Shows or hides the progress bar and "Let Me In" button based on the inProgress parameter.
-     *
-     * @param inProgress Boolean indicating if the operation is in progress.
-     */
-
-    void  setInProgress(boolean inProgress){
-        if(inProgress) {
-            progressBar.setVisibility(View.VISIBLE);
-            letMeInBtn.setVisibility(View.GONE);
-        }else{
-            progressBar.setVisibility(View.GONE);
-            letMeInBtn.setVisibility(View.VISIBLE);
+         if(binding.loginUsername.getText().toString().trim().isEmpty()){
+            showToast(("Please enter your Email or your Username"));
+            return false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.loginUsername.getText().toString()).matches()) {
+            showToast(("Please enter Valid Email"));
+            return false;
+        }else if(binding.loginPassword.getText().toString().trim().isEmpty()) {
+            showToast("Please enter your Password");
+            return false;
+        }else if(binding.loginConfirmedPassword.getText().toString().trim().isEmpty()){
+            showToast("Please confirm your Password");
+            return false;
+        }else if(!binding.loginPassword.getText().toString().equals(binding.loginConfirmedPassword.getText().toString())) {
+            showToast("Password not the same");
+            return false;
+        }else {
+            return true;
         }
     }
+
+
+
+
+
+
+
+
+    private void loading(Boolean isLoading) {
+        if(isLoading) {
+            binding.textSignIn.setVisibility(View.INVISIBLE);
+            binding.loginProgressBar.setVisibility(View.VISIBLE);
+        }else {
+            binding.loginProgressBar.setVisibility(View.INVISIBLE);
+            binding.textSignIn.setVisibility(View.VISIBLE);
+
+        }
+    }
+
+
+
+
+
 }
